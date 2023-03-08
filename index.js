650003dcdd41e81070ffa66f82fa4da2ad7badfc
x6Nenko <107570914+x6Nenko@users.noreply.github.com>
2023-03-08 20:17:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 400,
+    spaceBetween: 20,
+    grabCursor: true,
+    slidesPerView: 2,
+    centeredSlides: true,
+    initialSlide: 1
+  });
